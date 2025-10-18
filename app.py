f242bc405356702140414d8db06f5fd0e25436dc
Santiago <santiagoarteche7@gmail.com>
2025-10-18 11:17:46 -0300

    refactor and delete product

--- app.py
+++ app.py
@@ -10,15 +10,8 @@ def start():
     label = Label(root, text="Granja Martinez", font=("Arial", 24))
     label.pack()
 
-    product_name = Label(root, text="Nombre producto:", font=("Arial", 18))
-    name_entry = Entry(root, font=("Arial", 18))
-    product_name.pack()
-    name_entry.pack()
-
-    product_price = Label(root, text="Precio producto:", font=("Arial", 18))
-    price_entry = Entry(root, font=("Arial", 18))
-    product_price.pack()
-    price_entry.pack()
+    name_entry = create_field(root, "Nombre producto:")
+    price_entry = create_field(root, "Precio producto:")
 
     total_var = DoubleVar(value=0.0)
 
@@ -43,17 +36,36 @@ def add_entry(root, name_entry, price_entry, total_var: DoubleVar, total_label: 
         err = Label(root, text=f"Precio inválido: '{price_text}' — se tomó como 0", fg="red", font=("Arial", 12))
         err.pack()
 
-    product_name = Label(root, text=name or "(sin nombre)", font=("Arial", 18))
+    new_product(root, name, price, total_var, total_label)
+
+    update_total(price, total_var, total_label)
+
+    name_entry.delete(0, 'end')
+    price_entry.delete(0, 'end')
+
+def update_total(price: float, total_var: DoubleVar, total_label: Label):
+    total_var.set(total_var.get() + price)
+    total_label.config(text="Total: " + str(total_var.get()))
+
+
+def create_field(root, label_text):
+    label = Label(root, text=label_text, font=("Arial", 18))
+    entry = Entry(root, font=("Arial", 18))
+    label.pack()
+    entry.pack()
+    return entry
+
+def new_product(root, name, price, total_var: DoubleVar, total_label: Label):
+    product_name = Label(root, text=name or "(sin nombre)", font=("Arial", 18) )
     product_name.pack()
 
     product_price = Label(root, text=f"{price:.2f}", font=("Arial", 18))
     product_price.pack()
 
-    total_var.set(total_var.get() + price)
-    total_label.config(text="Total: " + str(total_var.get()))
-
-    name_entry.delete(0, 'end')
-    price_entry.delete(0, 'end')
+    delete_button = Button(root, text="Eliminar", font=("Arial", 14),
+                           command=lambda: [product_name.destroy(), product_price.destroy(), delete_button.destroy(),
+                                            update_total(-price, total_var, total_label)])
+    delete_button.pack()
 
 
 start()
